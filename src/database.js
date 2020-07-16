0f2c303e935dc33938ea1a190baa835b46570d53
YouDyZ <61251510+YouDyZ@users.noreply.github.com>
2020-07-16 21:00:18 +0200

    adding sql, fixing emit

---
+++ src/database.js
@@ -0,0 +1,7 @@
+const sql = require("sql");
+const {app} = require("electron");
+
+//TODO - SQL Database connectrion
+
+sql.setDialect("mysql");
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -49,3 +49,8 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+ipcMain.on("testsend", (event, args) => {
+  console.log(args);
+  event.reply("testresponse", "response")
+})
